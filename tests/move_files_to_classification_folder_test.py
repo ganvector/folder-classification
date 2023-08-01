589e5a1dc9a625a749f6ee5273b3e74f7930e970
Vitor Nascimento <vitor.gan21@gmail.com>
2023-07-31 23:38:52 -0300

    test: add script for what steps should be made

---
+++ tests/move_files_to_classification_folder_test.py
@@ -0,0 +1,6 @@
+# Criar a pasta (test_folder)
+# Rodar monitoramento
+# Adicionar arquivo de txt na pasta
+# Verificar se arquivo foi movido
+# Prosseguir para proximo tipo de arquivo
+# Excluir pasta com todos os seus componentes internos
